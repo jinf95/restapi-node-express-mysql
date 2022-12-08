a01aa3508c75146bf7256db334f3d0a4a7522cd3
Jinf95 <jinf95n@gmail.com>
2022-12-08 10:35:44 -0300

    apirest node-express-mysql

---
+++ src/routes/language.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { methods as languageController } from "../controllers/languages.controller";
+
+const router = Router();
+
+router.get("/", languageController.getLanguages)
+router.get("/:id", languageController.getLanguage)
+router.post("/", languageController.addLanguages)
+router.delete("/:id", languageController.deleteLanguage)
+router.put("/:id", languageController.updateLanguage)
+
+
+
+
+export default router;
